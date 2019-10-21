abdb1e2e300e664e211774da3f8d20f874a4925b
ego6ua <ego6ua@users.noreply.github.com>
2019-10-21 23:05:34 +0200

    zad_11

---
+++ 00-CourseIntroduction/zad_11.py
@@ -0,0 +1,8 @@
+login = "marek"
+hasło = "m-123"
+Login_podany = input("Podaj login:")
+Hasło_podane = input("Podaj hasło:")
+if login == Login_podany and hasło == Hasło_podane:
+    print("Podane dane są prawidłowe")
+else:
+    print("Podane dane są nieprawidłowe")
