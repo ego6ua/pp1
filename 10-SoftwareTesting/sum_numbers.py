c39684d2610537b9b05de549cbac3ee8f7ca657b
s-115-24 <s-115-24@gumiforest.ae.krakow.pl>
2020-01-20 14:38:48 +0100

    10

--- 10-SoftwareTesting/sum_numbers.py
+++ 10-SoftwareTesting/sum_numbers.py
@@ -1,15 +1,20 @@
 # Sumuje liczby nturalne parzyste z przedziału <from_n,to_n>
 def sum_even(from_m,to_n):
     sum = 0
-    for i in range(from_m,to_n):
+    if from_m<0:
+            from_m = 0
+    if to_n<0:
+        to_n=0 
+    for i in range(from_m,to_n+1):
+                   
         if i%2 == 0: # liczba parzysta
             sum += i
     return sum
 
 def main():
-    m = 1
-    n = 5
-    print(f"Suma liczb naturalnych parzystych z przedziału <{m},{n}> wynosi {sum_even(1,5)}")
+    m = 2
+    n = 8
+    print(f"Suma liczb naturalnych parzystych z przedziału <{m},{n}> wynosi {sum_even(m,n)}")
 
 if __name__ == "__main__":
     main()
--- 10-SoftwareTesting/test_sum_numbers.py
+++ 10-SoftwareTesting/test_sum_numbers.py
@@ -27,8 +27,20 @@ class TestSumEven(unittest.TestCase):
         n = 2
         result = sum_even(m,n)
         self.assertEqual(result, 0)
-
-
+        
+    def test_sum_even_mmnijsze(self):
+        #obliczanie sumy liczb naturalnych parzystych z przedziału <m,n>, gdy m<0
+        m = -20
+        n = 5
+        result = sum_even(m,n)
+        self.assertEqual(result, 6)
+        
+    def test_sum_even_rzeczywisty(self):
+        #obliczanie sumy gdy m i/lub n są liczbami rzeczywistymi
+        m = 2.1234
+        n = 5.234
+        ressult = sum_even(m,n)
+        self.assertEqual(result, )       
 
 if __name__ == '__main__':
     unittest.main()
